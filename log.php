fbf9d5b445d9e53095d0b0af307aa567ec17f2db
Mickey Teerawath Maunsopah <30790322+Nanandmic567@users.noreply.github.com>
2019-05-01 15:34:58 +0700

    Update log.php

--- log.php
+++ log.php
@@ -1,11 +1,13 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$production = isset($url["host"]);
 date_default_timezone_set(“Asia/Bangkok”);
 $id = $_REQUEST[‘id’];
 $text = $_REQUEST[‘text’];
-$serverName = “us-cdbr-iron-east-02.cleardb.net”;
-$userName = “b07243a7245e6e”;
-$userPassword = “0766a58f”;
-$dbName = “heroku_c8e3b40b80e5798”;
+$serverName = ($production) ? $url["host"] : “localhost”;
+$userName = ($production) ? $url["user"] : “root”;
+$userPassword = ($production) ? $url["pass"] : “”;
+$dbName = ($production) ? substr($url["path"], 1) : “log”;
 $connect = mysqli_connect($serverName,$userName,$userPassword,$dbName) or die (“connect error”.mysqli_error());
 mysqli_set_charset($connect, “utf8”);
 $query =”INSERT INTO log_chat (user_line_id,TEXT,DATE) VALUE(‘12234’,’mickey’,NOW())”;
