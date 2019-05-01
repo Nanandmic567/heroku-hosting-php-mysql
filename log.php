8c97e128a1dc528ec4cd29ac032d73b047a128f5
Mickey Teerawath Maunsopah <30790322+Nanandmic567@users.noreply.github.com>
2019-05-01 16:36:56 +0700

    Update log.php

--- log.php
+++ log.php
@@ -8,7 +8,7 @@ $userPassword = "0766a58f";
 $dbName = "heroku_c8e3b40b80e5798";
 $connect = mysqli_connect($serverName,$userName,$userPassword,$dbName) or die ("connect error".mysqli_error());
 mysqli_set_charset($connect, "utf8");
-$query ="INSERT INTO log_chat (user_line_id,TEXT,DATE) VALUE('12234','mickey',NOW())";
+$query = "INSERT INTO line_log(user_id,text,date_time) VALUE ('$id','$text',NOW())";
 $resource = mysqli_query($connect,$query) or die ("error".mysqli_error());
 ?>
 __SILENT___
