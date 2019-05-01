ea596df5bba43fd63d9d271bc04b82287e20d579
Mickey Teerawath Maunsopah <30790322+Nanandmic567@users.noreply.github.com>
2019-05-01 15:07:39 +0700

    Create log.php

---
+++ log.php
@@ -0,0 +1,14 @@
+<?php
+date_default_timezone_set(“Asia/Bangkok”);
+$id = $_REQUEST[‘id’];
+$text = $_REQUEST[‘text’];
+$serverName = “us-cdbr-iron-east-02.cleardb.net”;
+$userName = “b07243a7245e6e”;
+$userPassword = “0766a58f”;
+$dbName = “heroku_c8e3b40b80e5798”;
+$connect = mysqli_connect($serverName,$userName,$userPassword,$dbName) or die (“connect error”.mysqli_error());
+mysqli_set_charset($connect, “utf8”);
+$query =”INSERT INTO log_chat (user_line_id,TEXT,DATE) VALUE(‘12234’,’mickey’,NOW())”;
+$resource = mysqli_query($connect,$query) or die (“error”.mysqli_error());
+?>
+__SILENT___
