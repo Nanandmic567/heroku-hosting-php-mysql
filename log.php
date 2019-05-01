9387cefeb28dbfe7d6eef62eef720319f736e033
Mickey Teerawath Maunsopah <30790322+Nanandmic567@users.noreply.github.com>
2019-05-01 15:55:54 +0700

    Update log.php

--- log.php
+++ log.php
@@ -1,16 +1,16 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $production = isset($url["host"]);
-date_default_timezone_set(“Asia/Bangkok”);
-$id = $_REQUEST[‘id’];
-$text = $_REQUEST[‘text’];
-$serverName = ($production) ? $url["host"] : “localhost”;
-$userName = ($production) ? $url["user"] : “root”;
-$userPassword = ($production) ? $url["pass"] : “”;
-$dbName = ($production) ? substr($url["path"], 1) : “log”;
-$connect = mysqli_connect($serverName,$userName,$userPassword,$dbName);
-mysqli_set_charset($connect, “utf8”);
-$query ="INSERT INTO log_chat (user_line_id,TEXT,DATE) VALUE(‘12234’,’mickey’,NOW())";
-$resource = mysqli_query($connect,$query) or die (“error”.mysqli_error());
+date_default_timezone_set("Asia/Bangkok");
+$id = $_REQUEST['id'];
+$text = $_REQUEST['text'];
+$serverName = ($production) ? $url["host"] : "localhost";
+$userName = ($production) ? $url["user"] : "root";
+$userPassword = ($production) ? $url["pass"] : "";
+$dbName = ($production) ? substr($url["path"], 1) : "log";
+$connect = mysqli_connect($serverName,$userName,$userPassword,$dbName) or die ("connect error".mysqli_error());;
+mysqli_set_charset($connect, "utf8");
+$query ="INSERT INTO log_chat (user_line_id,TEXT,DATE) VALUE('12234','mickey',NOW())";
+$resource = mysqli_query($connect,$query) or die ("error".mysqli_error());
 ?>
 __SILENT___
