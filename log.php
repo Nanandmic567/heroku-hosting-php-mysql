b040f947073fff6b91944f3eb2a32042453e629b
Mickey Teerawath Maunsopah <30790322+Nanandmic567@users.noreply.github.com>
2019-05-01 15:59:54 +0700

    Update log.php

--- log.php
+++ log.php
@@ -8,7 +8,7 @@ $serverName = ($production) ? $url["host"] : "localhost";
 $userName = ($production) ? $url["user"] : "root";
 $userPassword = ($production) ? $url["pass"] : "";
 $dbName = ($production) ? substr($url["path"], 1) : "log";
-$connect = mysqli_connect($serverName,$userName,$userPassword,$dbName) or die ("connect error".mysqli_error());;
+$connect = mysqli_connect($serverName,$userName,$userPassword,$dbName) or die ("connect error".mysqli_error());
 mysqli_set_charset($connect, "utf8");
 $query ="INSERT INTO log_chat (user_line_id,TEXT,DATE) VALUE('12234','mickey',NOW())";
 $resource = mysqli_query($connect,$query) or die ("error".mysqli_error());
